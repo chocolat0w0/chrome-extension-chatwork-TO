9214a1050a2b26b86715285d913afd56b6e06126
chocoffice <s-taguchi@biglobe.co.jp>
2016-04-05 18:13:27 +0900

    わたしたID, altを使用するよう修正

--- app/scripts.babel/modules/tool-icon-creater.js
+++ app/scripts.babel/modules/tool-icon-creater.js
@@ -8,11 +8,11 @@ export function create(id, label, imageFileName) {
     var icon = document.createElement('img');
     icon.src = chrome.extension.getURL('images/' + imageFileName);
     var iconWrapper = document.createElement('li');
-    iconWrapper.id = '_replaceButton';
+    iconWrapper.id = id;
     iconWrapper.setAttribute('class', '_showDescription');
     iconWrapper.setAttribute('style', 'display: inline-block; padding-top: 3px;');
     iconWrapper.setAttribute('role', 'button');
-    iconWrapper.setAttribute('aria-label', 'Toの宛名を削除したよ');
+    iconWrapper.setAttribute('aria-label', label);
     iconWrapper.appendChild(icon);
     return iconWrapper;
 }
